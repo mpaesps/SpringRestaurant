35370a9b573159a239d91623860fc8e2bf528db7
gquerentino <gquerentino@outlook.com>
2023-07-27 15:31:41 -0300

    Adding database config

--- src/main/java/br/com/guilherme/springrestaurant/SpringRestaurantApplication.java
+++ src/main/java/br/com/guilherme/springrestaurant/SpringRestaurantApplication.java
@@ -8,8 +8,6 @@ public class SpringRestaurantApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, World!!");
-
 		SpringApplication.run(SpringRestaurantApplication.class, args);
 	}
 
