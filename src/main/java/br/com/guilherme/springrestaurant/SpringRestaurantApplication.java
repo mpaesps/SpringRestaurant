7d96684da790b64163159d8c5e41d73fe63f32c3
gquerentino <gquerentino@outlook.com>
2023-07-27 08:03:28 -0300

    First commit

---
+++ src/main/java/br/com/guilherme/springrestaurant/SpringRestaurantApplication.java
@@ -0,0 +1,16 @@
+package br.com.guilherme.springrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestaurantApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World!");
+
+		SpringApplication.run(SpringRestaurantApplication.class, args);
+	}
+
+}
